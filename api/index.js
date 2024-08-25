8a28f8b43504988ebf0dfd7611778546c6c27686
JayaSanthosh7066 <jayasanthoshamalakanti@gmail.com>
2024-08-25 12:10:55 +0530

    seocnd

--- api/index.js
+++ api/index.js
@@ -2,50 +2,40 @@ import express from "express";
 
 const app = express();
 app.use(express.json());
+// Sample user information
+const userInfo = {
+  user_id: "j",
+  email: "john@xyz.com",
+  roll_number: "ABCD123",
+};
+
+// POST endpoint
 app.post("/bfhl", (req, res) => {
   const { data } = req.body;
-  console.log(data);
+
   if (!Array.isArray(data)) {
-    return res.status(400).json({
-      is_success: false,
-      message: "Invalid input format",
-    });
+    return res.status(400).json({ is_success: false, error: "Invalid input" });
   }
 
-  const numbers = [];
-  const alphabets = [];
-  let highestLowercaseAlphabet = "";
-
-  data.forEach((item) => {
-    if (!isNaN(item)) {
-      numbers.push(item);
-    } else if (/[a-zA-Z]/.test(item)) {
-      alphabets.push(item);
-      if (item === item.toLowerCase() && item > highestLowercaseAlphabet) {
-        highestLowercaseAlphabet = item;
-      }
-    }
-  });
+  const numbers = data.filter((item) => !isNaN(item));
+  const alphabets = data.filter((item) => isNaN(item));
+  const highestLowercaseAlphabet = alphabets
+    .filter((char) => /^[a-z]$/.test(char))
+    .sort()
+    .slice(-1);
 
   res.json({
     is_success: true,
-    user_id: "john_doe_17091999",
-    email: "john@xyz.com",
-    roll_number: "ABCD123",
+    ...userInfo,
     numbers,
     alphabets,
-    highest_lowercase_alphabet: highestLowercaseAlphabet
-      ? [highestLowercaseAlphabet]
-      : [],
+    highest_lowercase_alphabet: highestLowercaseAlphabet,
   });
 });
-
 app.get("/bfhl", (req, res) => {
-  res.json({
-    operation_code: 1,
-  });
+  res.status(200).json({ operation_code: 1 });
 });
 
 app.listen(3001, () => {
-  console.log(`Server is listening at port ${3001}`);
+  console.log(`Server running on port 3001`);
 });
